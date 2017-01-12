d614ee7773ee2beb2a39deec4ea84b6e66c2db84
Pablo <pabloimrik17@gmail.com>
2017-01-12 23:01:20 +0100

    Parte 2

---
+++ Assets/enemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySpawner : MonoBehaviour {
+
+	public GameObject enemyToSpawn;
+
+	public bool canSpwan = true;
+
+	public float enemySpawnTime = 1.0f;
+	public float maxX = 28.0f;
+	public float startingY = 20.0f;
+
+	float randomX;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(spawnEnemyTimer());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator spawnEnemyTimer() {
+		spawnEnemy();
+		yield return new WaitForSeconds (enemySpawnTime);
+	}
+
+	void spawnEnemy() {
+		randomX = Random.Range(-maxX, maxX);
+
+		Instantiate(enemyToSpawn, new Vector3(randomX, startingY, 0), Quaternion.identity);
+	}
+
+}
