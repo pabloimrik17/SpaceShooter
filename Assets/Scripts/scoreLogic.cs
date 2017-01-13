dd98f0c1d5da28e6e560e46d35f6c606a6e1477e
Pablo <pabloimrik17@hotmail.com>
2017-01-13 15:19:43 +0100

    Parte 3

--- Assets/Scripts/scoreLogic.cs
+++ Assets/Scripts/scoreLogic.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scoreLogic : MonoBehaviour {
 
+	Text text;
+
+	public int score;
 	// Use this for initialization
 	void Start () {
-		
+		score = 0;
+		text = GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
